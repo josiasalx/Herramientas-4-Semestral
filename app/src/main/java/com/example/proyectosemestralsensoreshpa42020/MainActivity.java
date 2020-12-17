5ef21018e285a01ea1f15bfc17cfa11e622eb6d9
Josias Aponte <josiasalx11@gmail.com>
2020-12-17 13:01:21 -0500

    Se agregó el layout de presentación, al igual que el botón que redirija a la presentación y el botón de regresar al menú principal. Faltan los demás datos de la presentación, esta es una prueba a ver si el push funciona. Además, se le coloca la función salir al botón.

--- app/src/main/java/com/example/proyectosemestralsensoreshpa42020/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralsensoreshpa42020/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.proyectosemestralsensoreshpa42020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button presentación, salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+presentación =findViewById(R.id.presentación);
+presentación.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(MainActivity.this,Presentacion.class));
+        finish();
+
+    }
+});
+salir=findViewById(R.id.salir);
+salir.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        finish();
+        Intent intent= new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+});
     }
 }
